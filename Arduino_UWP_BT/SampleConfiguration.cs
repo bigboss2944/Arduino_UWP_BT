45a42c9615346066a61aa0afd5311d1aa608f852
fafa.noel29 <fafa.noel29@gmail.com>
2020-03-30 20:42:13 +0200

    First commit

---
+++ Arduino_UWP_BT/SampleConfiguration.cs
@@ -0,0 +1,65 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace Arduino_UWP_BT
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Bluetooth Rfcomm Chat Sample";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario()
+            {
+                Title = "Chat Client",
+                ClassType = typeof(Scenario1_ChatClient)
+            },
+
+            new Scenario()
+            {
+                Title = "Button",
+                ClassType = typeof(Scenario2_Button)
+            },
+
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+
+    /// <summary>
+    /// Class containing Attributes and UUIDs that will populate the SDP record.
+    /// </summary>
+    class Constants
+    {
+        // The Chat Server's custom service Uuid: 34B1CF4D-1069-4AD6-89B6-E161D79BE4D8
+        public static readonly Guid RfcommChatServiceUuid = Guid.Parse("00001101-0000-1000-8000-00805F9B34FB");
+
+        // The Id of the Service Name SDP attribute
+        public const UInt16 SdpServiceNameAttributeId = 0x100;
+
+        // The SDP Type of the Service Name SDP attribute.
+        // The first byte in the SDP Attribute encodes the SDP Attribute Type as follows :
+        //    -  the Attribute Type size in the least significant 3 bits,
+        //    -  the SDP Attribute Type value in the most significant 5 bits.
+        public const byte SdpServiceNameAttributeType = (4 << 3) | 5;
+
+        // The value of the Service Name SDP attribute
+        public const string SdpServiceName = "Bluetooth Rfcomm Chat Service";
+    }
+}
